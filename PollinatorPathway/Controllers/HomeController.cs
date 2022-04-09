c3fa888bd52edd2f6807860a3952a178ff49af61
BlankaPeller <blanka.peller@outlook.com>
2022-04-08 17:40:12 -0700

    Finished View

--- PollinatorPathway/Controllers/HomeController.cs
+++ PollinatorPathway/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult AdminPortal()
     {
         return View();
     }
--- PollinatorPathway/Program.cs
+++ PollinatorPathway/Program.cs
@@ -46,7 +46,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=AdminPortal}/{id?}");
 app.MapRazorPages();
 
 app.Run();
