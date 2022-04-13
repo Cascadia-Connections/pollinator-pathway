fc8d39146c0e3bc8a159c0c32e47b08a74eca128
BlankaPeller <blanka.peller@outlook.com>
2022-04-12 17:10:06 -0700

    added authorization

--- PollinatorPathway/Controllers/HomeController.cs
+++ PollinatorPathway/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PollinatorPathway.ViewModels;
 
@@ -13,6 +14,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [Authorize]
     public IActionResult AdminPortal()
     {
         return View();
