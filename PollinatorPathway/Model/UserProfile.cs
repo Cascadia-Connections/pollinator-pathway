008d36ad2c32257882dfc4ee44183a682789504a
sovian <aouldah@gmail.com>
2022-04-14 11:55:11 -0700

    Add DbContect for app database

---
+++ PollinatorPathway/Model/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace PollinatorPathway.Model
+{
+    public class UserProfile
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+    }
+}
