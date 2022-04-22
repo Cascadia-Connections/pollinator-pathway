789d51577eaa45136e0bc08d20b74ec7c2466abb
sovian <aouldah@gmail.com>
2022-04-21 20:16:07 -0700

    Add image model

---
+++ PollinatorPathway/Model/UploadedImage.cs
@@ -0,0 +1,11 @@
+﻿namespace PollinatorPathway.Model
+{
+    public class UploadedImage
+    {
+            public int ImageId { get; set; }
+            public string imageUrl { get; set; }
+            public Boolean IsApprovced { get; set; }
+            public UserProfile Profile { get; set; }
+
+    }
+}
