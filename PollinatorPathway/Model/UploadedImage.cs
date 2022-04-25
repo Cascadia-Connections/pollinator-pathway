212d8cb444461121b7254b3ea9aa1d2c1c6b2a2b
BlankaPeller <blanka.peller@outlook.com>
2022-04-24 22:46:59 -0700

    Revert "Adds first version of the API"
    This reverts commit 26f6c2a96450b9094c3307322fa1defc0bd273f1.

--- PollinatorPathway/Model/UploadedImage.cs
+++ PollinatorPathway/Model/UploadedImage.cs
@@ -1,16 +1,11 @@
-﻿using Microsoft.Build.Framework;
-
-namespace PollinatorPathway.Model
+﻿namespace PollinatorPathway.Model
 {
     public class UploadedImage
     {
-        public long Id { get; set; }
-        public string ImageUrl { get; set; }
-        public Boolean IsApprovced { get; set; }
-        public UserProfile Profile { get; set; }
-
-        [Required]
-        public IFormFile File { get; set; }
+            public int Id { get; set; }
+            public string imageUrl { get; set; }
+            public Boolean IsApprovced { get; set; }
+            public UserProfile Profile { get; set; }
 
     }
 }
